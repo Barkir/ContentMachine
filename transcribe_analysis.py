0752e06ecfee7ac06f74b4d9e6a6bf7e57a627c6
Barkir <r.barinov2006@gmail.com>
2025-09-03 23:50:03 +0300

    Added Term frequency

--- transcribe_analysis.py
+++ transcribe_analysis.py
@@ -2,9 +2,14 @@
 
 import nltk
 from nltk.corpus import treebank
-
+from nltk.probability import FreqDist
 
 lines = "".join(open("posts/5ada3554-04d3-483d-b0ce-3b33351643bd.post").readlines())
 tokens = nltk.word_tokenize(lines)
 tagged = nltk.pos_tag(tokens)
-print(tagged)
+# print(tagged)
+
+fdist = FreqDist(tokens)
+
+for word, frequency in fdist.items():
+    print(f"{word}: {frequency}")
